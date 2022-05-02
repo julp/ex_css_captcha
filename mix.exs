07d80817c2d63f1c4c7d7f47324c07b8a9017eca
julp <e2c2be7ed0f2f336@gmail.com>
2022-05-02 19:46:23 +0200

    drop `Mix.Config` (deprecated since Elixir 1.9.0) in favor of `Config`

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExCSSCaptcha.MixProject do
     [
       app: :ex_css_captcha,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
